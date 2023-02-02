f9a8623bd34b60dd514dd5f697784d6ee358e223
Jimit Dholakia <76764841+JimitSoni18@users.noreply.github.com>
2023-02-02 09:54:42 +0530

    Add files via upload

---
+++ MyProject/home/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# makemigrations - create changes and save in a file (0001_initial)
+# migrate - apply the pending changes created by makemigrations
+
+# Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=122)
+    email = models.CharField(max_length=122)
+    phone = models.CharField(max_length=122)
+    desc = models.TextField()
+    date = models.DateField()
+
+    def __str__(self):
+        return self.name
